4737a5bfd29b010206ad6207a4a3b94442808723
toffus <63292373+toffus@users.noreply.github.com>
2025-05-23 23:58:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route("/transcript")
 def get_transcript():
-    video_id = request.args.get("id")  # <-- DAS ist entscheidend!
+    video_id = request.args.get("id")
     if not video_id:
         return jsonify({"error": "Bitte gib eine Video-ID an (Parameter: id)"}), 400
     try:
@@ -19,7 +19,6 @@ def get_transcript():
         import traceback
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == "__main__":
     app.run()
